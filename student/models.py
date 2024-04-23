954669a3fe07a4ea94a64f962330af18d1ccd23f
Sarvarbek <150660166+TechSarvarbek@users.noreply.github.com>
2024-04-23 14:57:59 +0500

    Update models.py

--- student/models.py
+++ student/models.py
@@ -7,7 +7,7 @@ class Student(models.Model):
     school = models.CharField(max_length=15)
     schoolClass = models.CharField(max_length=5)
     telegramId = models.IntegerField(unique=True)
-    image = models.TextField()
+    image = models.TextField(null=True, blank=True)
     fullname = models.CharField(max_length=20)
     phone = models.IntegerField()
     motto = models.CharField(max_length=50)
